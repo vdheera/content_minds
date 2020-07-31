af56cfe282351426c8f8435ccdd5bfbb822b0450
vdheera <vdheera@umich.edu>
2020-07-31 02:37:19 -0400

    user registration

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
+//Connect Database
+
+connectDB();
+
+app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("API running"));
+
+//Define Routes
+app.use("/users", require("./routes/api/users"));
+app.use("/auth", require("./routes/api/auth"));
+app.use("/profile", require("./routes/api/profile"));
+app.use("/posts", require("./routes/api/posts"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
